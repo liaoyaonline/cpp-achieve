15839c82f8216ddfdc3a2dfd90ccca33f4614a98
liaoyaonline <71174500014@stu.ecnu.edu.cn>
2019-01-06 16:40:13 +0800

    日常更新

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function push_all() #将标签上传到github
 {
-    git add /home/liaoya/github/linux-vim/Bookmarks
-    git commit -m "日常更新标签"
+    git add /home/liaoya/github/cpp实现/*
+    git commit -m "日常更新"
     git push
 }
-  push_bookmarks
+  push_all
 
